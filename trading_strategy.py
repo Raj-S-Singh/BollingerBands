d25bb29a065803a10e5c8ef4e3dd2abc7128b60d
Raj Shree Singh <rssingh1999david@gmail.com>
2019-06-10 13:59:42 +0530

    Add files via upload

--- trading_strategy.py
+++ trading_strategy.py
@@ -1,43 +1,87 @@
-from Bollinger_Bands.trades import get_trades, get_csticks, get_closePriceNTime,\
-    get_bands
-from test.test_binop import isnum
-def trade_long(t,close_list,bands,budget):
+from trades import get_trades, get_csticks, get_closePriceNTime,get_bands
+import matplotlib.pyplot as plt
+def trade_long(t,close_list,bands,budget,markers):
     buy_price=float(close_list[t])
     buy_quant=budget/buy_price
     stoploss_price=0.95*buy_price
     
     for i in range(t+1,len(close_list)):
         curr_price=float(close_list[i])
-        if curr_price<=float(bands[1][i]) or curr_price<=stoploss_price:
-            print(curr_price/buy_price)
+        markers.append(0)
+        if curr_price<stoploss_price :
+            #print(curr_price/buy_price)
             sell_price =curr_price
             profit=(sell_price-buy_price)*buy_quant
             return profit,i+1
+        if 0.95*curr_price>stoploss_price:
+            stoploss_price=0.95*curr_price
     return "Not sold yet",len(close_list)    
-        
-trade_data=get_trades('BNBUSDT','4h','1000')
-csticks = get_csticks(trade_data)
-t_list,t_time=get_closePriceNTime(trade_data)
-bands = get_bands(t_list) 
-count=0
-trade_success=0
-i=19
-profit=0.0
-profit_list=[]
-budget=1000
-while i<len(csticks[3]):
-    cp=float(csticks[3][i])
-    if cp>=float(bands[0][i]):
-        
-        profit,j=trade_long(i,csticks[3],bands,budget)
-        
-        i=j
-        count+=1
-        if isnum(profit):
-            budget+=profit
-            profit_list.append(profit)
-            if profit>0:
-                trade_success+=1
-    else:
-        i+=1        
-print(count,trade_success,'\n',profit_list,'\n', sum(profit_list))        
+    
+def trade_short(t,close_list,bands,budget,markers):
+    sell_price=float(close_list[t])
+    sell_quant=budget/sell_price
+    stoploss_price=0.95*sell_price
+    
+    for i in range(t+1,len(close_list)):
+        curr_price=float(close_list[i])
+        markers.append(0)
+        if curr_price>stoploss_price:
+            print(curr_price/sell_price)
+            buy_price =curr_price
+            profit=(sell_price-buy_price)*sell_quant
+
+            return profit,i+1
+        if 1.0*curr_price<stoploss_price:
+            stoploss_price=0.95*curr_price
+    return "Not sold yet",len(close_list)    
+
+def backtest_():
+    for inox in range(0,1):
+        trade_data=get_trades('BNBUSDT','4h','1000',str(1543604400000+(inox*2592000000)),str(1543604400000+((inox+1)*2592000000)))
+        csticks = get_csticks(trade_data)
+        t_list,t_time=get_closePriceNTime(trade_data)
+        bands = get_bands(t_list) 
+        #print(bands[2])
+        count1=0
+        count2=0
+        trade_success1=0
+        trade_success2=0
+        i=19
+        profit1=0.0
+        flag=0
+        profit1_list=[]
+        profit2_list=[]
+        budget=1000
+        markers=[]
+        print(len(csticks[3]))
+        while i<len(csticks[3]):
+            cp=float(csticks[3][i])
+            if cp>=float(bands[0][i]):
+                
+                markers.append(cp)
+                profit1,j=trade_long(i,csticks[3],bands,budget,markers)
+                
+                i=j
+                count1+=1
+                if type(profit1) != str:
+                    budget+=profit1
+                    profit1_list.append(profit1)
+                    if profit1>0:
+                        trade_success1+=1
+        #    elif cp<float(bands[2][i]):
+        #         markers.append(cp)
+        #         profit2,q=trade_short(i,csticks[3],bands,budget,markers)
+        #         i=q
+        #         count2+=1
+        #         if type(profit2) != str:
+        #             budget+=profit2
+        #             profit2_list.append(profit2)
+        #             if profit2>0:
+        #                 trade_success2+=1
+            
+            else:
+                markers.append(0)
+                i+=1        
+        print(count1,trade_success1,'\n',profit1_list,'\n', sum(profit1_list))
+        #print(count2,trade_success2,'\n',profit2_list,'\n', sum(profit2_list))
+    return markers
