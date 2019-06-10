42704d3a6f887646f0a5820d4b5dc0e6888b45bb
Raj Shree Singh <rssingh1999david@gmail.com>
2019-06-10 14:01:02 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from Bollinger_Bands.trades import get_trades,plot_graph,get_bands,get_csticks,get_closePriceNTime
-
-trade_data=get_trades('BNBUSDT','4h','1000')
+from trades import get_trades,plot_graph,get_bands,get_csticks,get_closePriceNTime
+import trading_strategy  as TS
+trade_data=get_trades('BNBUSDT','4h','1000','sasda','asas')
 csticks = get_csticks(trade_data)
 
 t_list,t_time=get_closePriceNTime(trade_data)
 bands = get_bands(t_list) 
-plot_graph(bands,csticks,t_time)   
+markers=TS.backtest_()
+plot_graph(markers,bands,csticks,t_time)   
+
